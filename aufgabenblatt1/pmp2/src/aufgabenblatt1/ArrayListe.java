1511e17a234ea056308222cb3eb436a798dfc59f
kafawi <derkalle@web.de>
2015-10-26 11:11:09 +0400

    Do major things in aufgabe1.2 and 1.4

--- aufgabenblatt1/pmp2/src/aufgabenblatt1/ArrayListe.java
+++ aufgabenblatt1/pmp2/src/aufgabenblatt1/ArrayListe.java
@@ -1,41 +1,195 @@
 /**
- * Praktikum TIPR2, WS 15/16
+ * Praktikum TIPM2, WS 15/16
  * Gruppe: Alexander Mendel (alexander.mendel@haw-hamburg.de)
  *         Karl-Fabian Witte (karl-fabian.witte@haw-hamburg.de)
  * Aufgabe: Aufgabenblatt 1, Aufgabe 4 ("Generics")
- *
  */
-package aufgabenblatt1;
 
-public class ArrayListe<T> {
-	int  anzahlElemente;
-	private Object[] elemente;
-	public ArrayListe(int arrayGroesse){
-		anzahlElemente = 0;
-		elemente = new Object [arrayGroesse];
-	}
-	public void hinzufuegen(T item){
-		elemente[getAnzahlElemente()] = elemente [getAnzahlElemente()+1];
-	}
-	public T get(int index){
-		T wert = null;
-		elemente[index] = wert;
-		return wert;
-	}
-	public void entfernen (T item){
-		elemente[getAnzahlElemente()] = elemente [getAnzahlElemente()-1];
-	}
-	public void entferneElementAnIndex(int index){
-		elemente[index] = null;
-		anzahlElemente --;
-	}
-	public int getAnzahlElemente(){
-		return anzahlElemente;
-	}
-	public String toString(){
-		
-	}
-	public T getKleinstesElement(){
-		
-	}
+package arbeitsblatt1;
+
+/**
+ * Eine ContainerKlasse, um die Generics zu üben.
+ * 
+ * @author Alexander Mendel und Karl-Fabain Witte
+ */
+@SuppressWarnings("unchecked")
+// Wenn Klasse die UpperBound verwedet:
+ public class ArrayListe<T extends Comparable<T>> {
+//public class ArrayListe<T> {
+  //----------------------------------------------------------------- Variablen+
+  
+  /**
+   * Anzahl der Elemente in der Liste
+   */
+  private int anzahlElemente;
+  
+  /**
+   * Array, welches alle 
+   */
+  private Object[] elemente;
+  
+  //--------------------------------------------------------------- Konstruktor
+  /**
+   * Konstruktor leere Liste
+   */
+  public ArrayListe(){
+    anzahlElemente=0;
+    elemente=null;
+  }
+  
+  //------------------------------------------------------------------ Methoden
+  /**
+   * Hängt an der Liste ein Element unten ran.
+   * @param element anzuhängende Element.
+   */
+  public void hinzufuegen(T element){
+    anzahlElemente++;
+    Object[] elementeNeu = new Object[anzahlElemente];
+    
+    // flache Kopie da wir ja überschreiben wollen
+    if(elemente!=null){
+      System.arraycopy(elemente, 0, elementeNeu, 0, elemente.length);
+    }
+    // hänge neues element unten dran
+    elementeNeu[ elementeNeu.length - 1 ]= element;
+    
+    elemente = elementeNeu;
+  }
+  
+  /**
+   * Holt ein Element aus der Liste.
+   * 
+   * @param index Stelle im Array, an der das gewünschte Element liegt
+   * @return gewünschte Element
+   * @throws IndexOutOfBoundsException
+   */
+  public T get(int index) throws IndexOutOfBoundsException{    
+    if( elemente != null ){
+      return (T)elemente[index];
+    } 
+    return null;
+  }
+  
+  /**
+   * Entfernt ein Element falls vorhanden aus der Liste.
+   * @param elementWeg zu entfernende Element
+   */
+  public void entfernen(T elementWeg){
+    if( elemente != null ){
+      int index=anzahlElemente-1;
+      for (index=anzahlElemente-1; index > 0; index-- ){    
+        if ( elemente[index].equals(elementWeg) ){
+          
+          anzahlElemente--;
+          // Wenn die Liste Leer ist, setze Array auf null
+          if (anzahlElemente == 0){
+            elemente = null;
+            return;
+          }
+          
+          //flache Kopie
+          Object[] elementeNeu = new Object[anzahlElemente];
+          // Vor elementWeg
+          System.arraycopy(elemente, 0, elementeNeu, 0, index);
+          // Hinter elementWeg
+          System.arraycopy(elemente, index+1, elementeNeu, index,
+              elemente.length - (index + 1) );
+          elemente = elementeNeu;
+          return;
+        }
+      }
+    }
+  }
+  
+  /**
+   * Entfernt das Element an einer bestimmten Stelle.
+   * @param indexWeg Stelle in der Liste, an der Das Element gelöscht wird.
+   * @throws IndexOutOfBoundsException
+   */
+  public void entferneElementAnIndex(int indexWeg) 
+      throws IndexOutOfBoundsException{
+    
+    if (elemente == null || indexWeg < 0 || indexWeg >= elemente.length){
+      throw new IndexOutOfBoundsException(); 
+    }
+    
+    anzahlElemente--;
+    // Wenn die Liste Leer ist, setze Array auf null
+    if (anzahlElemente == 0){
+      elemente = null;
+      return;
+    }
+    
+    // flache Kopie
+    Object[] elementeNeu = new Object[anzahlElemente];
+    // Vor indexWeg
+    System.arraycopy(elemente, 0, elementeNeu, 0, indexWeg);
+    // Hinter indexWeg
+    System.arraycopy(elemente, indexWeg+1, elementeNeu, indexWeg,
+        elemente.length - (indexWeg + 1) );
+    elemente = elementeNeu;
+  }
+  
+  //-------------------------------------------------------- Getter und toSting
+  /**
+   * Getter anzahlElemente
+   * @return Die Anzahl der Elemente, die im Array stehen.
+   */
+  public int getAnzahlElemente(){
+    return anzahlElemente;
+  }
+  
+  /**
+   * toString
+   * @return beschreibender text
+   */
+  public String toString(){
+    String text = "";
+    text += String.format("%d Elemente vom Typ ", anzahlElemente);
+    text += get(0).getClass().getTypeName();
+    
+    return text;
+  }
+  
+  //--------------------------------------------------- UpperBounds in Methoden
+  /**
+   * Gibt das kleinste Element aus der Liste wieder.
+   * Dafür wird eine Vergleichsmöglichkeit benötigt (Comparable).
+   * @return kleinste Element der Liste
+   */
+  
+  // Wenn Klasse die UpperBound verwedet:
+  public T getKleinstesElement(){
+  //public <T extends Comparable<T>> T getKleinestesElement(){
+    T kleinstesElement = null;
+    
+    if( elemente != null ){
+      int indexKlein = 0;
+      for (int index = 0; index < anzahlElemente; index++){
+        if (((T)elemente[indexKlein]).compareTo((T)elemente[index]) == 1){
+          indexKlein = index;
+        }
+      }
+      kleinstesElement = (T)elemente[indexKlein];
+    }
+    return kleinstesElement;
+    
+  }
+  
+  
+  // ------------------------------------------------------------ Integer Summe
+  
+  /**
+   * Summiert die Werte einer Liste von Ganzzahlen.
+   * @param intListe Liste von Ganzzahlen
+   * @return Summe aller Ganzzahlen der Liste
+   */
+  public static int summe(ArrayListe<Integer> intListe){
+    int sum = 0;
+    for(int index = 0; index < intListe.getAnzahlElemente(); index++){
+      sum += intListe.get(index).intValue();
+    }
+    return sum;
+  }
+ 
 }
--- aufgabenblatt1/pmp2/src/aufgabenblatt1/Student.java
+++ aufgabenblatt1/pmp2/src/aufgabenblatt1/Student.java
@@ -1,72 +1,138 @@
 /**
- * Praktikum TIPR2, WS 15/16
+ * Praktikum TIPM2, WS 15/16
  * Gruppe: Alexander Mendel (alexander.mendel@haw-hamburg.de)
  *         Karl-Fabian Witte (karl-fabian.witte@haw-hamburg.de)
- * Aufgabe: Aufgabenblatt 1, Aufgabe 2 ("Wiederholung PM1/PT")
- *
+ * Aufgabe: Aufgabenblatt 1, Aufgabe 2 ("Wiederholung")
  */
-package aufgabenblatt1;
+package arbeitsblatt1;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class Student implements Comparable<Student>{
-//----------Variablen---------
-private String vorname;
-private String nachname;
-private int matrikelnummer;
-private List<Pruefungsleistung> plListe;
-//---------Konstruktor--------
-public Student (String vorname, String nachname, int matrikelnummer){
-  this.vorname = vorname;
-  this.nachname = nachname;
-  this.matrikelnummer = matrikelnummer;
-  plListe = new ArrayList<Pruefungsleistung>();
-}
-
-//------------Getter---------
-public String getVorname() {
-	return vorname;
-}
-public int getMatrikelnummer() {
-	return matrikelnummer;
-}
-public String getNachname() {
-	return nachname;
-}
-public List<Pruefungsleistung> getPlListe() {
-	return plListe;
-}
-//-------------Setter---------
-public void setVorname(String vorname) {
-	this.vorname = vorname;
-}
-public void setNachname(String nachname) {
-	this.nachname = nachname;
-}
-public void setMatrikelnummer(int matrikelnummer) {
-	this.matrikelnummer = matrikelnummer;
-}
-public void setPlListe(List<Pruefungsleistung> plListe) {
-	this.plListe = plListe;
-}
-
-@Override
-public int compareTo(Student tStudent) {
- 
-if(tStudent != null){	  
-  Student other = (Student) tStudent;
-	
-  if (getMatrikelnummer() > other.getMatrikelnummer()){ 
-    return 1;
+/**
+ * Informationen eines Studenten an einer Universität. 
+ * 
+ * @author Alexander Mendel und Karl-Fabain Witte
+ */
+public class Student implements Comparable<Student> {
+  
+  //--------------------------------------------------------- Klassen-Variablen
+  /**
+   * Objektzähler für die Matrikelnummer
+   */
+  private static int matrikelnummerZaehler = 0;
+  
+  //---------------------------------------------------------- Objekt-Variablen
+  /**
+   * Vorname des Studenten
+   */
+  private String vorname;
+  
+  /**
+   * Nachname des Studenten
+   */
+  private String nachname;
+  
+  /**
+   * eindeutige Identifikationsnummer
+   * Matrikelnummer wird erstellt im Konstruktor mit MatrikelnummerZaehler.
+   */
+  private final int matrikelnummer;
+  
+  /**
+   * Liste mit allen Prüfungen 
+   */
+  private ArrayList<Pruefungsleistung> pruefungsleistungsListe;
+  
+  //--------------------------------------------------------------- Konstruktor
+  /**
+   *  Konstruktor
+   * 
+   * @param modulName Name des Moduls
+   * @param note erbrachte Leistung in der Modulprüfung
+   */
+  public Student(String vorname, String nachname){
+    this.vorname = vorname; 
+    this.nachname = nachname;
+    matrikelnummer = matrikelnummerZaehler;
+    pruefungsleistungsListe = new ArrayList<Pruefungsleistung>();
+    
+    matrikelnummerZaehler++;
   }
-  else if (getMatrikelnummer() < other.getMatrikelnummer()){ 
+  
+  //-------------------------------------------------------------------- Getter
+  /**
+   * Getter vorname
+   * @return Vorname des Studenten
+   */
+  public String getVorname(){
+    return vorname;
+  }
+  
+  /**
+   * Getter nachname
+   * @return Nachname des Studenten
+   */
+  public String getNachname(){
+    return nachname;
+  }
+  
+  /**
+   * Getter matrikelnummer
+   * @return eindeutige Identifikationsnummer
+   */
+  public int getMatrikelnummer(){
+    return matrikelnummer;
+  }
+  
+  /**
+   * Getter pruefungsleistungsListe
+   * @return Liste aller universitären erbrachten Leitungen
+   */
+  public ArrayList<Pruefungsleistung> getPruefungsleistungsListe(){
+    return pruefungsleistungsListe;
+  }
+  
+  //-------------------------------------------------------------------- Setter
+  /**
+   * Setter vorname
+   * @param vorname Vorname des Studenten
+   */
+  public void setVorname(String vorname){
+    this.vorname = vorname;
+  }
+  
+  /**
+   * Setter nachname
+   * @param nachname Nachname des Studenten
+   */
+  public void setNachname(String nachname){
+    this.nachname = nachname;
+  }
+  
+  /**
+   * Setter pruefungsleistungsListe
+   * @param pruefungsleistungsListe Liste aller Prüfungen
+   */
+  public void setVorname(ArrayList<Pruefungsleistung> pruefungsleistungsListe){
+    this.pruefungsleistungsListe = pruefungsleistungsListe;
+  }
+  
+  //----------------------------------------------------------------- Overrides
+  @Override
+  public int compareTo(Student student) {
+    if(student != null){
+      if( matrikelnummer < student.getMatrikelnummer() ){
+        return -1;
+      }
+      else if( matrikelnummer == student.getMatrikelnummer() ){
+        return 0;
+      }
+      else if( matrikelnummer > student.getMatrikelnummer() ){
+        return 1;
+      }
+    }
     return -1;
   }
-  else if (getMatrikelnummer() == other.getMatrikelnummer()){
-    return 0;  
-  }
+
 }
-  return -1;
-}
-}
+//------------------------------------------------------------------------- EOF
