11a897c9e9431e04d685edd57ce553b3e5f491a1
kafawi <kafa.witte@web.de>
2015-11-12 09:25:30 +0100

    add Aufgabe 2.2 and starts 2.3

---
+++ aufgabenblatt1/pmp2/src/aufgabenblatt2/Rennauto.java
@@ -0,0 +1,36 @@
+/**
+ * Praktikum TIPM2, WS 15/16
+ * Gruppe: Alexander Mendel (alexander.mendel@haw-hamburg.de)
+ *         Karl-Fabian Witte (karl-fabian.witte@haw-hamburg.de)
+ * Aufgabe: Aufgabenblatt 2, Aufgabe 1 ("Lamdas")
+ */
+
+package aufgabenblatt2;
+
+/**
+ * Diese Klasse ist ein Rennauto.
+ * @author Alexander Mendel und Karl-Fabian Witte
+ *
+ */
+public class Rennauto extends Thread {
+  
+  
+  
+  
+  //--------------------------------------------------------------- KONSTRUKTOR
+  /**
+   * KONSTRUKTOR
+   */
+  public Rennauto(){
+    
+    
+    
+  }
+  
+  
+  @Override
+  public void run(){
+    
+  }
+
+}
