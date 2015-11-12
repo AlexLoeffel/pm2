5dc5517f98dd02b4bfd6ce21814541010ca6499e
kafawi <kafa.witte@web.de>
2015-11-12 07:45:13 +0100

    Adding some tests to 2.1

--- aufgabenblatt1/pmp2/src/aufgabenblatt2/DoubleDoubleZuDoubleTest.java
+++ aufgabenblatt1/pmp2/src/aufgabenblatt2/DoubleDoubleZuDoubleTest.java
@@ -1,3 +1,9 @@
+/**
+ * Praktikum TIPM2, WS 15/16
+ * Gruppe: Alexander Mendel (alexander.mendel@haw-hamburg.de)
+ *         Karl-Fabian Witte (karl-fabian.witte@haw-hamburg.de)
+ * Aufgabe: Aufgabenblatt 2, Aufgabe 1 ("Lamdas")
+ */
 package aufgabenblatt2;
 
 import static org.junit.Assert.*;
@@ -11,26 +17,85 @@ import org.junit.Test;
  *
  */
 public class DoubleDoubleZuDoubleTest {
-  private Double[]     x = { 1., 2., 3., 4., 5., 6., 7., 8. };
-  private Double[]     y = { 1., 2., 3., 4., 5., 6., 7., 8. };
+  final private int    LEN = 10;
+  private Double[]     x   = new Double[LEN];
+  private Double[]     y   = new Double[LEN];
 
   DoubleDoubleZuDouble tester;
 
   @Test
-  public void testWerteAusAddition() {
+  public void testWerteAusPlus() {
+
     tester = (x, y) -> x + y;
-    for (int i = 0; i < x.length; i++) {
-      assertEquals(tester.werteAus(x[i], y[i]), y[i] + x[i], 1e-4);
+
+    for (int i = 0; i < LEN; i++) {
+      x[i] = 100 * Math.random();
+      y[i] = 100 * Math.random();
+    }
+
+    for (int i = 0; i < LEN; i++) {
+      assertEquals(tester.werteAus(x[i], y[i]), x[i] + y[i], 1E-5);
+    }
+  }
+
+  @Test
+  public void testWerteAusMinus() {
+
+    tester = (x, y) -> x - y;
+
+    for (int i = 0; i < LEN; i++) {
+      x[i] = 100 * Math.random();
+      y[i] = 100 * Math.random();
+    }
+
+    for (int i = 0; i < LEN; i++) {
+      assertEquals(tester.werteAus(x[i], y[i]), x[i] - y[i], 1E-5);
+    }
+  }
+
+  @Test
+  public void testWerteAusMal() {
+
+    tester = (x, y) -> x * y;
+
+    for (int i = 0; i < LEN; i++) {
+      x[i] = 100 * Math.random();
+      y[i] = 100 * Math.random();
+    }
+
+    for (int i = 0; i < LEN; i++) {
+      assertEquals(tester.werteAus(x[i], y[i]), x[i] * y[i], 1E-5);
+    }
+  }
+
+  @Test
+  public void testWerteAusDurch() {
+
+    tester = (x, y) -> x / y;
+
+    for (int i = 0; i < LEN; i++) {
+      x[i] = 100 * Math.random();
+      y[i] = 100 * Math.random();
+    }
+
+    for (int i = 0; i < LEN; i++) {
+      assertEquals(tester.werteAus(x[i], y[i]), x[i] / y[i], 1E-5);
     }
   }
 
   @Test
   public void testWerteAusNullstellen() {
-    // gelichung ist a = x*b +y
-    // Nulstellen bei a=0 -> b= y/x
-    tester = (a, b) -> b / a;
-    for (int i = 0; i < x.length; i++) {
-      assertEquals(tester.werteAus(x[i], y[i]), y[i] / x[i], 1e-4);
+    // gelichung ist y = a*x + b
+    // Nulstellen bei y=0 -> x= -b/a
+    tester = (a, b) -> -b / a;
+
+    for (int i = 0; i < LEN; i++) {
+      x[i] = 100 * Math.random();
+      y[i] = 100 * Math.random();
+    }
+
+    for (int i = 0; i < LEN; i++) {
+      assertEquals(tester.werteAus(x[i], y[i]), -y[i] / x[i], 1E-5);
     }
   }
 
